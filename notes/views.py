9f03edd06dd98cd82bc84cbd2fd8a785389adc0a
wayne71 <wayne.hatter1971@gmail.com>
2024-04-29 12:21:08 -0500

    clean up

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .forms import NotesForm
 from .models import Notes
 
+class NotesUpdateView(UpdateView):  
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+    
 class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
