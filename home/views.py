97a1832ef5942c4d559c7e25719f952701ed263e
wayne71 <wayne.hatter1971@gmail.com>
2024-04-29 03:41:56 -0500

    user authentication

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 # Create your views here.
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
