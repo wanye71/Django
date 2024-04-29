944b7f1a8a215abfa0ff19731d1893b2fa1ec039
wayne71 <wayne.hatter1971@gmail.com>
2024-04-29 15:25:55 -0500

    display loggin user data

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,10 @@
-from typing import List
+from typing import Any, List
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -12,10 +14,14 @@ class NotesDeleteView(DeleteView):
     success_url = '/smart/notes'
     template_name = "notes/notes_delete.html"
     
-class NotesUpdateView(UpdateView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+    
+class NotesUpdateView(LoginRequiredMixin,UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
 class NotesCreateView(CreateView):
     model = Notes
