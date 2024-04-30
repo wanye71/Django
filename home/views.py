5b98aba72cdc8860b8e2c0356b4b6515cddee810
wayne71 <wayne.hatter1971@gmail.com>
2024-04-30 07:00:09 -0500

    Added login and logout pages

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 
-from datetime import datetime
+class LogoutInterfaceView(LogoutView):
+    template_name= 'home/logout.html'
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
